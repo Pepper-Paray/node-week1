453359d8774c154cd48e7b6bfe4bfa86306b3825
Mary (William pepper) Paray <pandapup3000@gmail.com>
2026-02-26 18:32:38 +0000

    added stuff for week1day2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ProductCard from './ProductCard.jsx';
+import ProductCard from './ProductCard.jsx';
+import ProductCard from './ProductCard.jsx';
 
 function HelloWorld() {
   return (
-    <h1>Hello World, I am Mary 'Pepper'Paray !! </h1>
+    <h1>Hello World, I am Mary 'Pepper' Paray !!</h1>
   );
 }
 
 export default HelloWorld;
+
 
